2aca12b216e4303e4b76af884c39ff6a23b33f93
Richard Chou <konami99@hotmail.com>
2024-11-29 11:39:12 +1100

    added modal

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -2,15 +2,18 @@ import { setStatusBarHidden } from "expo-status-bar"
 import ScreenWrapper from "../../components/ScreenWrapper"
 import { useAuth } from "../../contexts/AuthContext"
 import { supabase } from "../../lib/supabase"
-import { StyleSheet, Text, View } from "react-native"
+import { Alert, StyleSheet, Text, View } from "react-native"
 import { hp, wp } from '../../helpers/common'
 import { theme } from '../../constants/theme'
 import Button from '../../components/Button'
-import { ScrollView } from "react-native"
+import { ScrollView, Pressable } from "react-native"
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { useState } from "react"
+import { Modal } from 'react-native'
 
 const Home = () => {
   const { setAuth } = useAuth();
+  const [modalVisible, setModalVisible] = useState(false);
   
   const onLogout = async() => {
     const { error } = await supabase.auth.signOut();
@@ -19,12 +22,36 @@ const Home = () => {
     }
   }
 
+  const onPress = () => {
+    setModalVisible(true);
+  }
+
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         <View style={styles.titleSection}>
           <Text style={styles.title}>Habits</Text>
-          <FontAwesome5 name="plus" size={24} color="black" />
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible)
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+          <Pressable onPress={onPress}>
+            <FontAwesome5 name="plus" size={24} color="black" />
+          </Pressable>
         </View>
         <View style={styles.header}>
           <Text>November</Text>
@@ -71,6 +98,49 @@ const Home = () => {
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    height: hp(80),
+    width: wp(90),
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+
   habitItem: {
     backgroundColor: theme.colors.primaryDark,
     height: hp(17),
